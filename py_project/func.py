ab70a06a1d8eb89b9797a79eb164bf6cbaaba9a9
Ruskol31 <rk31@yandex.ru>
2024-03-12 18:57:33 +0300

    add func difference

--- py_project/func.py
+++ py_project/func.py
@@ -2,11 +2,10 @@ def sum(a,b):
     c = int(a + b)
     return c
 
+def difference(a,b):
+    c = int(a - b)
+    return c
 
-print("Введите слагаемые")
-a = int(input("a="))
-b = int(input("b="))
-c = sum(a,b)
-print(c)
+
 
 
